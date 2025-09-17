3d3c6d2d3832021aaeddce51cc0ba527dcd3cb49
spud <92900806+jamespud@users.noreply.github.com>
2025-09-17 22:42:14 +0800

    add other modules

--- tinystore-domain-gateway/src/main/java/com/github/spud/tinystore/gateway/GatewayApplication.java
+++ tinystore-domain-gateway/src/main/java/com/github/spud/tinystore/gateway/GatewayApplication.java
@@ -1,16 +1,17 @@
-package com.github.spud.tinystore.cart;
+package com.github.spud.tinystore.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Spud
- * @date 2025/9/13
+ * @date 2025/9/17
  */
+
 @SpringBootApplication
-public class CartApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CartApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 }
