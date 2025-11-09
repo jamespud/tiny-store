c8305b9d82ef13480d4a12a0c380e371ed298a77
spud <92900806+jamespud@users.noreply.github.com>
2025-11-09 21:33:22 +0800

    fix: clean code

--- tinystore-domain-inventory/src/main/java/com/github/spud/tinystore/inventory/messaging/events/ConfirmedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.github.spud.tinystore.inventory.messaging.events;
-
-/**
- * 确认事件（占位）
- */
-public class ConfirmedEvent extends StockEvent {
-
-}
-
--- tinystore-domain-inventory/src/main/java/com/github/spud/tinystore/inventory/messaging/kafka/StockListener.java
+++
@@ -1,9 +0,0 @@
-package com.github.spud.tinystore.inventory.messaging.kafka;
-
-/**
- * @author Spud
- * @date 2025/8/10
- */
-public class StockListener {
-
-}
